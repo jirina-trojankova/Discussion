27c92ae517f62b090cff658e0f049e7da85ec57f
jirina-trojankova <jirina.trojankova@seznam.cz>
2018-05-18 16:06:43 +0200

    Added a function for running a querries

--- database.php
+++ database.php
@@ -25,9 +25,9 @@ $database = "main";
  global $password;
 
 try {
-    $pdo = new PDO("mysql:host=".$servername.";dbname=."$database".; charset=utf8", $username, $password);
+    $pdo_connection = new PDO("mysql:host=".$servername.";dbname=."$database".; charset=utf8", $username, $password);
     // set the PDO error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 //insert to a database    
     $sql = "INSERT INTO discussion (nick, text, date)
@@ -38,7 +38,7 @@ try {
 
 
     // use exec() because no results are returned
-    $pdo->exec($sql);
+    $pdo_connection->exec($sql);
     $last_id = $conn->lastInsertId();
     echo "New record created successfully. Last inserted ID is: " . $last_id;    
     }
@@ -46,5 +46,30 @@ catch(PDOException $e)
     {
     echo "Connection failed: " . $e->getMessage();
     }
-return $pdo;
+return $pdo_connection;
+
+/**
+ * run a query
+ * 
+ * 
+ * runs a query using the predefined database
+ * 
+ * 
+ * @param string $query - query to be run
+ * @param array $values - values to be substituted for "?"
+ */
+function query($query, $values = []) {
+    global $database;
+
+    $pdo = connect($database);
+
+    $statement = $pdo->prepare($query); 
+    if(false === $statement->execute($values)) {
+        echo '<h1>MySQL error:</h1>'
+        var_dump($pdo->errorinfo());
+        exit();
+    }
+    $statement->setFetchMode(PDO::FETCH_ASSOC); //set the type of results
+    return $statement->fetchAll(); //get all results as an array
+}
 ?> 
